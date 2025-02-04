9a668b916df86048566ba8d3d8a3600bc62d3b16
sudedaka <sude__daka@hotmail.com>
2025-02-04 21:04:56 +0300

    menu item clicked app changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -36,6 +36,13 @@ const Navbar = () => {
     window.open("https://club.ieu.edu.tr/ogrenci-kulupleri", "_blank");
   };
 
+  // Menü dışında tıklama kontrolü
+  const handleOutsideClick = (e) => {
+    // Eğer menü dışına tıklanırsa menüyü kapat
+    if (e.target.closest(".mobile-menu")) return;
+    setIsMobileMenuOpen(false);
+  };
+
   return (
     <nav className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
       <motion.div
@@ -61,13 +68,12 @@ const Navbar = () => {
                 </a>
               </li>
             ))}
-         <button
-        className="primary-btn px-6 py-3 bg-blue-600 text-white rounded-lg text-lg font-semibold"
-        onClick={handleSignInClick}
-      >
-        Sign In
-      </button>
-
+            <button
+              className="primary-btn px-6 py-3 bg-blue-600 text-white rounded-lg text-lg font-semibold"
+              onClick={handleSignInClick}
+            >
+              Sign In
+            </button>
           </ul>
         </div>
 
@@ -86,7 +92,8 @@ const Navbar = () => {
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.3 }}
-          className="lg:hidden fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 z-40 flex flex-col items-center justify-center"
+          className="lg:hidden fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 z-40 flex flex-col items-center justify-center mobile-menu"
+          onClick={handleOutsideClick} // Menüyü kapatmak için tıklama kontrolü
         >
           <ul className="space-y-8 text-2xl">
             {NavbarMenu.map((menu) => (
@@ -106,18 +113,15 @@ const Navbar = () => {
                 </a>
               </motion.li>
             ))}
-              <motion.button
-        className="primary-btn text-5xl sm:text-3xl md:text-4xl px-8 py-3 bg-blue-600 text-white rounded-lg mt-4"
-        onClick={handleSignInClick}
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.3 }}
-      >
-        Sign In
-      </motion.button>
-
-
-
+            <motion.button
+              className="primary-btn px-8 py-3 bg-blue-600 text-white rounded-lg mt-4"
+              onClick={handleSignInClick}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.3 }}
+            >
+              Sign In
+            </motion.button>
           </ul>
         </motion.div>
       )}
